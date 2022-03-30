2bce67c9098bd30ef8687dd70ce52d489e89ccde
Moe Hendawi <mohamed.hendawi@wynnbet.com>
2022-03-30 18:21:37 -0400

    Saving progress

--- react-calculator/src/components/Button.js
+++ react-calculator/src/components/Button.js
@@ -3,7 +3,6 @@ import styles from "./Button.module.sass";
 
 const Button = ({ id, value, type, onClick }) => {
   const className = styles[id] || styles[type];
-  console.log({type, styles, className});
   return (
     <button id={id} className={className} onClick={onClick}>
       {value}
